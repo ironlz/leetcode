ead900bc803b93ba6cdb5445fb26f7f5f52cf2db
ironlz <ironlz@outlook.com>
2020-12-06 14:55:01 +0800

    Leetcode数组章节

---
+++ src/lessons/arrays/unit1/Init.java
@@ -0,0 +1,182 @@
+package lessons.arrays.unit1;
+
+import java.util.Arrays;
+
+/**
+ * 做数组类算法问题的时候，我们常常需要定义一个变量，
+ * 明确该变量的定义，并且在书写整个逻辑的时候，
+ * 要不停的维护住这个变量的意义。也特别需要注意初始值和边界的问题。
+ */
+public class Init {
+    /**
+     * 给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
+     * <p>
+     * 示例:
+     * <p>
+     * 输入: [0,1,0,3,12]
+     * 输出: [1,3,12,0,0]
+     * 说明:
+     * <p>
+     * 必须在原数组上操作，不能拷贝额外的数组。
+     * 尽量减少操作次数。
+     * <p>
+     * 解题思路
+     * 首先遍历一遍数列，用另个数列按顺序存储所有非 0 的元素，在将存储的非零元素按顺序复制到原数列中，空位补 0 即可。
+     * <p>
+     * 直观的解题思路新建额外的数组，不符合要求，但是对于我们下面的优化算法很有起始。
+     * <p>
+     * 简单的优化
+     * 只要把数组中所有的非零元素，按顺序给数组的前段元素位赋值，剩下的全部直接赋值 0。我们定义一个 nums 0...i 表示为非 0 元素的数组，之后在遍历数列的时候不断维护这个定义。
+     *
+     * @param nums
+     */
+    public void moveZeroes(int[] nums) {
+        int currentIndex = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                if (i == currentIndex) {
+                    currentIndex++;
+                } else {
+                    nums[currentIndex++] = nums[i];
+                }
+            }
+        }
+        for (int i = currentIndex; i < nums.length; i++) {
+            nums[i] = 0;
+        }
+    }
+
+    /**
+     * 给你一个数组 nums和一个值 val，你需要 原地 移除所有数值等于val的元素，并返回移除后数组的新长度。
+     * <p>
+     * 不要使用额外的数组空间，你必须仅使用 O(1) 额外空间并 原地 修改输入数组。
+     * <p>
+     * 元素的顺序可以改变。你不需要考虑数组中超出新长度后面的元素。
+     * <p>
+     * <p>
+     * <p>
+     * 示例 1:
+     * <p>
+     * 给定 nums = [3,2,2,3], val = 3,
+     * <p>
+     * 函数应该返回新的长度 2, 并且 nums 中的前两个元素均为 2。
+     * <p>
+     * 你不需要考虑数组中超出新长度后面的元素。
+     * 示例2:
+     * <p>
+     * 给定 nums = [0,1,2,2,3,0,4,2], val = 2,
+     * <p>
+     * 函数应该返回新的长度 5, 并且 nums 中的前五个元素为 0, 1, 3, 0, 4。
+     * <p>
+     * 注意这五个元素可为任意顺序。
+     * <p>
+     * 你不需要考虑数组中超出新长度后面的元素。
+     * <p>
+     * <p>
+     * 说明:
+     * <p>
+     * 为什么返回数值是整数，但输出的答案是数组呢?
+     * <p>
+     * 请注意，输入数组是以「引用」方式传递的，这意味着在函数里修改输入数组对于调用者是可见的。
+     * <p>
+     * 你可以想象内部操作如下:
+     * <p>
+     * // nums 是以“引用”方式传递的。也就是说，不对实参作任何拷贝
+     * int len = removeElement(nums, val);
+     * <p>
+     * // 在函数里修改输入数组对于调用者是可见的。
+     * // 根据你的函数返回的长度, 它会打印出数组中 该长度范围内 的所有元素。
+     * for (int i = 0; i < len; i++) {
+     * print(nums[i]);
+     * }
+     * <p>
+     * 作者：力扣 (LeetCode)
+     * 链接：https://leetcode-cn.com/leetbook/read/all-about-array/x9p1iv/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     *
+     * @param nums
+     * @param val
+     * @return
+     */
+    public int removeElement(int[] nums, int val) {
+        int currentIndex = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                if (i == currentIndex) {
+                    currentIndex++;
+                } else {
+                    nums[currentIndex++] = nums[i];
+                }
+            }
+        }
+        return currentIndex;
+    }
+
+    /**
+     * 给定一个排序数组，你需要在 原地 删除重复出现的元素，使得每个元素只出现一次，返回移除后数组的新长度。
+     * <p>
+     * 不要使用额外的数组空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。
+     * <p>
+     * <p>
+     * <p>
+     * 示例1:
+     * <p>
+     * 给定数组 nums = [1,1,2],
+     * <p>
+     * 函数应该返回新的长度 2, 并且原数组 nums 的前两个元素被修改为 1, 2。
+     * <p>
+     * 你不需要考虑数组中超出新长度后面的元素。
+     * 示例2:
+     * <p>
+     * 给定 nums = [0,0,1,1,1,2,2,3,3,4],
+     * <p>
+     * 函数应该返回新的长度 5, 并且原数组 nums 的前五个元素被修改为 0, 1, 2, 3, 4。
+     * <p>
+     * 你不需要考虑数组中超出新长度后面的元素。
+     * <p>
+     * <p>
+     * 说明:
+     * <p>
+     * 为什么返回数值是整数，但输出的答案是数组呢?
+     * <p>
+     * 请注意，输入数组是以「引用」方式传递的，这意味着在函数里修改输入数组对于调用者是可见的。
+     * <p>
+     * 你可以想象内部操作如下:
+     * <p>
+     * // nums 是以“引用”方式传递的。也就是说，不对实参做任何拷贝
+     * int len = removeDuplicates(nums);
+     * <p>
+     * // 在函数里修改输入数组对于调用者是可见的。
+     * // 根据你的函数返回的长度, 它会打印出数组中该长度范围内的所有元素。
+     * for (int i = 0; i < len; i++) {
+     * print(nums[i]);
+     * }
+     * <p>
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/remove-duplicates-from-sorted-array
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     *
+     * @param nums
+     * @return
+     */
+    public int removeDuplicates(int[] nums) {
+        int currentIndex = 0;
+        int count = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[currentIndex]) {
+                nums[++currentIndex] = nums[i];
+                ++count;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Init init = new Init();
+        int[] nums = {0, 1, 2, 2, 3, 0, 4, 2};
+        int element = init.removeElement(nums, 2);
+        System.out.println(Arrays.toString(nums));
+        System.out.println(element);
+    }
+}
