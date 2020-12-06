775eee9eabd8c3d6e40a5ae1cc103f0eef6732e2
ironlz <ironlz@outlook.com>
2020-12-06 20:09:13 +0800

    Leetcode数组章节

--- src/lessons/arrays/unit1/Init.java
+++ src/lessons/arrays/unit1/Init.java
@@ -95,9 +95,9 @@ public class Init {
      * 来源：力扣（LeetCode）
      * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
      *
-     * @param nums
-     * @param val
-     * @return
+     * @param nums 要计算的数组
+     * @param val  要移除的数
+     * @return 移除后剩余的数量
      */
     public int removeElement(int[] nums, int val) {
         int currentIndex = 0;
@@ -157,8 +157,8 @@ public class Init {
      * 链接：https://leetcode-cn.com/problems/remove-duplicates-from-sorted-array
      * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
      *
-     * @param nums
-     * @return
+     * @param nums 要计算的数字
+     * @return 移除后剩余的数字
      */
     public int removeDuplicates(int[] nums) {
         int currentIndex = 0;
@@ -172,10 +172,77 @@ public class Init {
         return count;
     }
 
+    /**
+     * 给定一个增序排列数组 nums ，你需要在 原地 删除重复出现的元素，使得每个元素最多出现两次，返回移除后数组的新长度。
+     * <p>
+     * 不要使用额外的数组空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。
+     * <p>
+     * <p>
+     * <p>
+     * 说明：
+     * <p>
+     * 为什么返回数值是整数，但输出的答案是数组呢？
+     * <p>
+     * 请注意，输入数组是以“引用”方式传递的，这意味着在函数里修改输入数组对于调用者是可见的。
+     * <p>
+     * 你可以想象内部操作如下：
+     * <p>
+     * // nums 是以“引用”方式传递的。也就是说，不对实参做任何拷贝
+     * int len = removeDuplicates(nums);
+     * <p>
+     * // 在函数里修改输入数组对于调用者是可见的。
+     * // 根据你的函数返回的长度, 它会打印出数组中该长度范围内的所有元素。
+     * for (int i = 0; i < len; i++) {
+     * print(nums[i]);
+     * }
+     * <p>
+     * <p>
+     * 示例 1：
+     * <p>
+     * 输入：nums = [1,1,1,2,2,3]
+     * 输出：5, nums = [1,1,2,2,3]
+     * 解释：函数应返回新长度 length = 5, 并且原数组的前五个元素被修改为 1, 1, 2, 2, 3 。 你不需要考虑数组中超出新长度后面的元素。
+     * 示例 2：
+     * <p>
+     * 输入：nums = [0,0,1,1,1,1,2,3,3]
+     * 输出：7, nums = [0,0,1,1,2,3,3]
+     * 解释：函数应返回新长度 length = 7, 并且原数组的前五个元素被修改为0, 0, 1, 1, 2, 3, 3 。 你不需要考虑数组中超出新长度后面的元素。
+     * <p>
+     * <p>
+     * 提示：
+     * <p>
+     * 0 <= nums.length <= 3 * 104
+     * -104 <= nums[i] <= 104
+     * nums 按递增顺序排列
+     * <p>
+     * 作者：力扣 (LeetCode)
+     * 链接：https://leetcode-cn.com/leetbook/read/all-about-array/x9nivs/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    public int removeDuplicatesAdvance(int[] nums) {
+        int currentIndex = 0;
+        int index = 0;
+        int repeatedCount = 0;
+        while (index < nums.length) {
+            if (index != currentIndex) {
+                if (nums[index] == nums[currentIndex] && repeatedCount < 1) {
+                    nums[++currentIndex] = nums[index];
+                    repeatedCount++;
+                } else if (nums[index] != nums[currentIndex]) {
+                    nums[++currentIndex] = nums[index];
+                    repeatedCount = 0;
+                }
+            }
+            index++;
+        }
+        return currentIndex + 1;
+    }
+
     public static void main(String[] args) {
         Init init = new Init();
-        int[] nums = {0, 1, 2, 2, 3, 0, 4, 2};
-        int element = init.removeElement(nums, 2);
+        int[] nums = {0, 0, 1, 1, 1, 1, 2, 3, 3};
+        int element = init.removeDuplicatesAdvance(nums);
         System.out.println(Arrays.toString(nums));
         System.out.println(element);
     }
